78bda58597bc74d33399f89a85c4dff73b411ecb
Dahyun (Dany) <88699158+Ko-dany@users.noreply.github.com>
2025-12-06 18:34:11 -0500

    Add data annotations for db constraints

--- Backend/TaskCollaborationAppAPI/TaskCollaborationAppAPI/Models/TaskItem.cs
+++ Backend/TaskCollaborationAppAPI/TaskCollaborationAppAPI/Models/TaskItem.cs
@@ -1,17 +1,25 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace TaskCollaborationAppAPI.Models
 {
     public class TaskItem
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; } 
         public string? Description { get; set; }
+        [Required]
         public TaskStatusTypes Status { get; set; }
+
         public int CreatedById { get; set; }
         public int? AssignedToId { get; set; }
+
+        [Required]
         public DateTime CreatedAt { get; set; }
+        [Required]
         public DateTime UpdatedAt { get; set; }
+        [Required]
         public bool IsArchived { get; set; }
         public DateTime? ArchivedAt { get; set; }
 
--- Backend/TaskCollaborationAppAPI/TaskCollaborationAppAPI/Models/User.cs
+++ Backend/TaskCollaborationAppAPI/TaskCollaborationAppAPI/Models/User.cs
@@ -1,13 +1,25 @@
-﻿namespace TaskCollaborationAppAPI.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskCollaborationAppAPI.Models
 {
+    [Index(nameof(Email), IsUnique = true)]
+    [Index(nameof(Username), IsUnique = true)]
     public class User
     {
+        [Key]   
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string PasswordHash { get; set; }
+        [Required]
         public string Role { get; set; }
+        [Required]
         public DateTime CreatedAt { get; set; }
     }
 }
