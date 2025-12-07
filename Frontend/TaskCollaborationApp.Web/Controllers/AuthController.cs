b43bd9ead0ebcbd7f4a7010c9c8a748377ff81d8
Dahyun (Dany) <88699158+Ko-dany@users.noreply.github.com>
2025-12-07 00:11:27 -0500

    Set up initial frontend module

---
+++ Frontend/TaskCollaborationApp.Web/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskCollaborationApp.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            // localStorage.clear()
+            return RedirectToAction("Login");
+        }
+    }
+}
