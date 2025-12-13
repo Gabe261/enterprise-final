3f8375ccc33439a94bb6b47a15ebaca5fd9d3938
gabrielsiewert <gabrielsiewert1@gmail.com>
2025-12-13 10:41:10 -0500

    Tried adding a call from the front end to reach the refresh endpoint.

--- Frontend/TaskCollaborationApp.Web/Controllers/HomeController.cs
+++ Frontend/TaskCollaborationApp.Web/Controllers/HomeController.cs
@@ -6,11 +6,20 @@ namespace TaskCollaborationApp.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
             ViewBag.Token = HttpContext.Session.GetString("JwtToken") ?? "No token";
             ViewBag.Email = HttpContext.Session.GetString("UserEmail") ?? "No email";
             ViewBag.Name = HttpContext.Session.GetString("UserName") ?? "No name";
+
+            ViewBag.ApiBaseUrl = _configuration["ApiBaseUrl"];
 
             return View();
         }
