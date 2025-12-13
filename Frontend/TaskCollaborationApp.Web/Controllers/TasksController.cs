6c9e847e7c4792ccef09d1b0b0cf511a07799e75
Dahyun (Dany) <88699158+Ko-dany@users.noreply.github.com>
2025-12-13 13:35:29 -0500

    Add initial task detail page and create form

---
+++ Frontend/TaskCollaborationApp.Web/Controllers/TasksController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskCollaborationApp.Web.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public TasksController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            SetViewBagData();
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            SetViewBagData();
+            ViewBag.TaskId = id;
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            SetViewBagData();
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            SetViewBagData();
+            ViewBag.TaskId = id;
+            return View();
+        }
+
+        private void SetViewBagData()
+        {
+            ViewBag.ApiBaseUrl = _configuration["ApiBaseUrl"];
+            ViewBag.Token = HttpContext.Session.GetString("JwtToken") ?? "No token";
+            ViewBag.Name = HttpContext.Session.GetString("Name") ?? "";
+            ViewBag.Email = HttpContext.Session.GetString("Email") ?? "";
+            ViewBag.UserName = HttpContext.Session.GetString("UserName") ?? "";
+            ViewBag.Role = HttpContext.Session.GetString("Role") ?? "";
+        }
+
+    }
+}
